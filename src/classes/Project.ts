e2a7f40a943c155d843c6a55ed5fab3fd31bdcf4
Alejandro Garrido <garridoca.alejandro@gmail.com>
2026-07-15 23:41:17 -0400

    Code commented

--- src/classes/Project.ts
+++ src/classes/Project.ts
@@ -34,6 +34,7 @@ export class Project implements IProject {
         this.setUI()
     }
 
+    // creates the project card UI
     setUI() {
         if (this.ui) {return}
         this.ui = document.createElement("div")
